46e6e7b3a91be03fb8227300697e48b80fc92bc6
Kevintata22 <kevintata22@gmail.com>
2022-10-05 19:08:51 -0300

    added kg

--- javacript.js
+++ javacript.js
@@ -59,6 +59,7 @@ function add(){
     let res2real = n2*ppp*real
     let n3 = Number(shipping.value)
     let res3 = n3*real
+    let kg = n2*0.45
     let finale = n*tax*real+(n2*ppp*real)+(n3*real)
     if (dollar.value.length == 0){
         window.alert(`Please enter a dollar amount`)
@@ -74,7 +75,7 @@ function add(){
     if (weight.value.length == 0){
         result3.innerHTML = `Weight not informed.`
     } else {
-        result3.innerHTML =  `Weight is ${n2}lbs. At $30/lb = <br> INTL Shipping is $${res2.toFixed(2)}. <br> INTL Shipping R$${res2real.toFixed(2)}`
+        result3.innerHTML =  `Weight is ${n2}lbs. ${n2}lbs = ${kg.toFixed(2)}kg <br> At $30/lb = <br> INTL Shipping is $${res2.toFixed(2)} <br> INTL Shipping R$${res2real.toFixed(2)}`
     }
     if (dollar.value.length >= 0){
         result4.innerHTML = `<br> Grand total R$${finale.toFixed(2)}`
