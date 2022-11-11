74e47c94daaeb6e3136da1f6db1a7cc9075ad0ad
Kevintata22 <kevintata22@gmail.com>
2022-11-11 14:16:01 -0300

    Remodeled
    Added API to update USD to BRL every 30 seconds.
    
    Changed interface to cleaner apple style design
    
    Made it perfect!

--- javacript.js
+++ javacript.js
@@ -1,46 +1,10 @@
-// function add(){
-//     var dollar = document.getElementById('dollar')
-//     var result = document.querySelector('div#result')
-//     var pretax = document.querySelector('div#pretax')
-//     var weight = document.querySelector('input#weight')
-//     var result2 = document.querySelector('div#result2')
-//     var result3 = document.querySelector('div#result3')
-//     var grand = document.querySelector('div#grand')
-//     if (dollar.value.length == 0){
-//         window.alert(`Please enter a dollar amount`)
-//     } else {
-//         let n = Number(dollar.value)
-//         let real = 5.18
-//         let tax = 1.08    
-//         let res1 = `$${n*tax*real}`
-//         result.innerHTML = `When converted to BRL, <br> ${n}&dollar; + 8% tax is = BRL R$${n*tax*real}`
-//         pretax.innerHTML = `Pre-Tax = R$${n*real}`
-//     }
-//     if (weight.value.length == 0){
-//         result2.innerHTML = `Weight not informed`
-//     } else {
-//         let n2 = Number(weight.value)
-//         let ppp = 30
-//         let res2  = `$${n2*ppp}`
-//         result2.innerHTML =  `Product weight is ${n2}lbs. Shipping will come out to ${res2}`
-//     }
-//     if (dollar.value.length >= 0 && weight.value >= 0){
-//         result3.innerHTML = `${res}`
-//     }
-// }
-// function weight(){
-//     var weight = document.querySelector('weight')
-//     var result2 = document.querySelector('div#result2')
-//     if (weight.value.length == 0){
-//         result2.innerHTML = `Weight not informed`
-//     } else {
-//         let n2= Number(weight.value)
-//         let ppp = 30
-//         result2.innerHTML = `Weight `
-//     }
-// }
+const getDollar = async () =>{
+    const res = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+    const data = await res.json()
+    var convert = data.USDBRL.ask
 
-function add(){
+
+    var container =document.getElementById('container-dollar')
     var dollar = document.getElementById('dollar')
     var result = document.querySelector('div#result')
     var pretax = document.querySelector('div#pretax')
@@ -50,34 +14,47 @@ function add(){
     var result4 = document.querySelector('div#result4')
     var shipping = document.querySelector('input#shipping')
     let n = Number(dollar.value)
-    let real = 5.50
-    let tax = 1.08    
-    let res1 = n*tax*real     
+    const real = convert*n
+    const tax = 1.06    
+    let res1 = n*tax*convert     
     let n2 = Number(weight.value)
-    let ppp = 30
+    const ppp = 30
     let res2  = n2*ppp
-    let res2real = n2*ppp*real
+    let res2real = n2*ppp*convert
     let n3 = Number(shipping.value)
-    let res3 = n3*real
+    let res3 = n3*convert
     let kg = n2*0.45
-    let finale = n*tax*real+(n2*ppp*real)+(n3*real)
-    if (dollar.value.length == 0){
-        window.alert(`Please enter a dollar amount`)
-    } else {
-        pretax.innerHTML = `Pre-Tax = R$${n*real.toFixed(2)}`
-        result.innerHTML = `${n}&dollar; + 8% tax = BRL R$${res1.toFixed(2)}`
-    }
+    let finale = n*tax*convert+(n2*ppp*convert)+(n3*convert)
+    let today = document.getElementById('brltoday')
+    let todayBrl = Number(convert)
+
+    today.innerHTML = `Dollar = R$${todayBrl.toFixed(2)}`
+
     if(shipping.value.length == 0){
-        result2.innerHTML = `Domestic shipping not informed.`
+        result2.innerHTML = ``
     } else {
         result2.innerHTML = `Domestic shipping is $${n3},<br> In BRL that comes out to R$${res3.toFixed(2)}`
     }
     if (weight.value.length == 0){
-        result3.innerHTML = `Weight not informed.`
+        // result3.innerHTML = `Weight not informed.`
     } else {
         result3.innerHTML =  `Weight is ${n2}lbs. ${n2}lbs = ${kg.toFixed(2)}kg <br> At $30/lb = <br> INTL Shipping is $${res2.toFixed(2)} <br> INTL Shipping R$${res2real.toFixed(2)}`
     }
     if (dollar.value.length >= 0){
         result4.innerHTML = `<br> Grand total R$${finale.toFixed(2)}`
     }
+
+    if (dollar.value.length == 0){
+        result.innerHTML = 'Please enter a Dollar amount.'
+        result2.innerHTML = 'Your result will go here'
+        result3.style.display = 'none'
+        result4.innerHTML = ' '
+    } else {
+        container.style.height = ''
+        // today.style.display = 'none'
+        pretax.innerHTML = `&dollar;${n} TO BRL = R$${real.toFixed(2)}`
+        result.innerHTML = `R&dollar;${real.toFixed(2)} + 8% tax = BRL R$${res1.toFixed(2)}`
+    }
+
 }
+getDollar()
